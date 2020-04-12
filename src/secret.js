1af3e7d29c3c4e5b09442e952bd3b275d4322b1a
Otavio <otaviocap@hotmail.com.br>
2020-04-11 22:46:38 -0300

    Stoped tracking secret.js

--- src/secret.js
+++
@@ -1,9 +0,0 @@
-/*
-*   This file contains all the information to connect to an Mongo Server
-*   if you want to use the aplication, just add put your credentials here
-*/
-
-module.exports = {
-    username: "username",
-    password: "passwrod"
-}
