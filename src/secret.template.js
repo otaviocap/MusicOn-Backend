93226687087760254bd453a4b6662c50c69daa86
Otavio <otaviocap@hotmail.com.br>
2020-06-16 19:43:43 -0300

    Added secret template

---
+++ src/secret.template.js
@@ -0,0 +1,10 @@
+/*
+*   This file contains all the information to connect to an MongoDB Server
+*   if you want to use the aplication, just add put your credentials here
+*/
+
+export default {
+    username: "Your MongoDB Username with read and write permissions",
+    password: "Your MongoDB Password",
+    key: "A key to verify identity in all important requests and db operations"
+}
